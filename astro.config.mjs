89bf7e470ba20c845b9dcfc0b7acd5a3a793f9b5
yeferson321 <131elyefer@gmail.com>
2026-04-16 07:21:59 -0500

    first committ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()],
   },
