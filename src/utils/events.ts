74fab7e554b19340f2db34549fcec5e0ec9a9b20
yeferson321 <131elyefer@gmail.com>
2026-04-25 13:05:26 -0500

    modal

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,39 +1,23 @@
-type EventMap = {
-  tabchange: {
-    tab: string
-  },
-  tabchange: {
-    tab: string
-  }
-}
-
 const target = new EventTarget()
 
-export function emit<K extends keyof EventMap>(
-  name: K,
-  data: EventMap[K]
-): void {
+type EventMap = {
+  	tabchange: { tab: string };
+  	toastshow: { id: string };
+};
 
-  window.dispatchEvent(
-    new CustomEvent(name, {
-      detail: data
-    })
-  )
+export function emit<K extends keyof EventMap>(name: K, data: EventMap[K]): void {
+  	target.dispatchEvent(
+  	  	new CustomEvent(name, {	detail: data })
+  	);
+};
 
-}
+export function on<K extends keyof EventMap>(name: K, callback: (data: EventMap[K]) => void): () => void {
+  	const handler = (event: Event) => {
+  	  	const { detail } = event as CustomEvent<EventMap[K]>;
+    	callback(detail);
+  	};
 
-export function on<K extends keyof EventMap>(
-  name: K,
-  callback: (data: EventMap[K]) => void
-): () => void {
+  	target.addEventListener(name, handler);
 
-  const handler = (event: Event) => {
-    const custom = event as CustomEvent<EventMap[K]>
-    callback(custom.detail)
-  }
-
-  target.addEventListener(name, handler)
-
-  return () => target.removeEventListener(name, handler)
-
-}
+  	return () => target.removeEventListener(name, handler);
+};
