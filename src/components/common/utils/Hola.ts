c6ce17351c568db8c28ab3ced21a145651a965da
yeferson321 <131elyefer@gmail.com>
2026-06-12 08:58:25 -0500

    feat: input

---
+++ src/components/common/utils/Hola.ts
@@ -0,0 +1,20 @@
+export const appendChildElement = (element: HTMLElement) => document.body.appendChild(element);
+export const removeElement = (element: HTMLElement) => element.remove();
+
+export const trapFocus = (element: HTMLElement, event: KeyboardEvent) => {
+    console.log("egw")
+    const focusables = Array.from(
+        element.querySelectorAll<HTMLElement>(':is(button, [href], input, select, textarea):not([disabled]):not([aria-hidden="true"])')
+    ).filter((HTMLElement) => HTMLElement.offsetParent !== null);
+
+    if (focusables.length === 0) return;
+
+    const firstFocusable = focusables[0];
+    const lastFocusable = focusables[focusables.length - 1];
+    const boundary = event.shiftKey ? firstFocusable : lastFocusable;
+
+    if (document.activeElement === boundary) {
+        event.preventDefault();
+        (event.shiftKey ? lastFocusable : firstFocusable).focus();
+    }
+};
