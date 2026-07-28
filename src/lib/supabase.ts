02f1c98dac3195f2f7d6b70bbfe0e5866396fc99
yeferson321 <131elyefer@gmail.com>
2026-07-28 16:54:35 -0500

    bytessssddqlal

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { env } from "cloudflare:workers";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseUrl = env.SUPABASE_URL;
+const supabaseKey = env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
