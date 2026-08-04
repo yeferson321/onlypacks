ce26d780d10d74cdeee3242b907106c370e1018f
yeferson321 <131elyefer@gmail.com>
2026-08-04 15:07:05 -0500

    prueba coockieersr

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 // src/middleware.ts
 import { defineMiddleware } from "astro:middleware";
 
-// export const onRequest = defineMiddleware((context, next) => {
-//     const sesionId = context.cookies.get("sesion_id")?.value;
+export const onRequest = defineMiddleware((context, next) => {
+    // const sesionId = context.cookies.get("sesion_id")?.value;
 
-//     if (!sesionId) {
-//         return context.redirect("/");
-//     }
+    // if (!sesionId) {
+    //     return context.redirect("/");
+    // }
 
-//     return next();
-// });
+    // return next();
+});
